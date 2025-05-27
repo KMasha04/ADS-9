f026d81ba278d3661cca0e59c6107d87d9f24461
Varvara Smirnova <varvarasmirnova110@gmail.com>
2025-05-27 21:03:12 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,19 @@
 // Copyright 2022 NNTU-CS
-#include "tree.h"
 #include <chrono>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 #include <vector>
+#include "tree.h"
 
-int factorial(int n) {
+static int factorial(int n) {
     int res = 1;
     for (int i = 2; i <= n; ++i) res *= i;
     return res;
 }
 
 int main() {
-    std::srand(std::time(nullptr));
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
     std::vector<char> sample = {'1', '2', '3'};
     PMTree tree(sample);
     std::cout << "All permutations:\n";
@@ -24,28 +24,23 @@ int main() {
     }
     for (int n = 3; n <= 8; ++n) {
         std::vector<char> alphabet(n);
-        for (int i = 0; i < n; ++i) 
+        for (int i = 0; i < n; ++i)
             alphabet[i] = '1' + i;
-        
         PMTree tree(alphabet);
         int total = factorial(n);
         int num = rand() % total + 1;
-        
         auto start = std::chrono::high_resolution_clock::now();
         auto all = getAllPerms(tree);
         auto end = std::chrono::high_resolution_clock::now();
         double t1 = std::chrono::duration<double>(end - start).count();
-        
         start = std::chrono::high_resolution_clock::now();
         getPerm1(tree, num);
         end = std::chrono::high_resolution_clock::now();
         double t2 = std::chrono::duration<double>(end - start).count();
-        
         start = std::chrono::high_resolution_clock::now();
         getPerm2(tree, num);
         end = std::chrono::high_resolution_clock::now();
         double t3 = std::chrono::duration<double>(end - start).count();
-        
         std::cout << "n=" << n << " getAllPerms: " << t1 << "s "
                   << "getPerm1: " << t2 << "s "
                   << "getPerm2: " << t3 << "s\n";
