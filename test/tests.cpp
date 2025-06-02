30911494a9cc0e2237521ada9d3169894a625215
KMasha04 <k.masha04030420@gmail.com>
2025-06-03 01:11:56 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -7,19 +7,19 @@
 
 TEST(ads9, test1) {
   PMTree tree(std::vector<char>{'1', '2', '3'});
-  std::vector<char> result = getPerm1(tree, 1);
+  std::vector<char> result = getPerm1(tree, 0);
   ASSERT_TRUE(result[0] == '1' && result[1] == '2' && result[2] == '3');
 }
 
 TEST(ads9, test2) {
   PMTree tree(std::vector<char>{'1', '2', '3'});
-  std::vector<char> result = getPerm2(tree, 2);
+  std::vector<char> result = getPerm2(tree, 1);
   ASSERT_TRUE(result[0] == '1' && result[1] == '3' && result[2] == '2');
 }
 
 TEST(ads9, test3) {
   PMTree tree(std::vector<char>{'1', '2', '3'});
-  std::vector<char> result = getPerm1(tree, 6);
+  std::vector<char> result = getPerm1(tree, 5);
   ASSERT_TRUE(result[0] == '3' && result[1] == '2' && result[2] == '1');
 }
 
